adf654b5ab773a9ef96c028c27dce66d008b8c47
Mark Reijn <mark.reijn.reijn@hva.nl>
2023-05-22 21:07:01 +0200

    Added base route.

--- routes/routes.php
+++ routes/routes.php
@@ -3,5 +3,6 @@
 /** @var Bramus\Router\Router $router */
 
 // Define routes here
+$router->setBasePath('/catering_api');
 $router->get('/test', App\Controllers\IndexController::class . '@test');
 $router->get('/', App\Controllers\IndexController::class . '@test');
